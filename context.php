7d45104eaf4ae1aecd3962725353a79f926cabf8
Saurabh Kumar <saurabhkumar3034@gmail.com>
2020-08-08 14:35:50 +0530

    Make code readable and understandable easily.

---
+++ context.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+</head>
+<body>
+	<div class="container-fluid">
+	    <div class="row">
+	         <div class="col-xs-4">
+	             <div class="thumbnail">
+	                <img src="images/m1.jpg" >
+	                  <div class="caption">
+	                    <h3>machin1</h3>
+	                    <p>choose the best in the world</p>
+	                  </div>
+	            </div>
+	        </div>
+
+	        <div class="col-xs-4">
+	             <div class="thumbnail">
+	                <img src="images/m2.jpg" >
+	                <div class="caption">
+	                    <h3>machine2</h3>
+	                    <p>choose the best in the world</p>
+	                  </div>
+	           </div>
+	        </div>
+	           
+	        <div class="col-xs-4">
+	             <div class="thumbnail">
+	                <img src="images/m4.jpg" >
+	                <div class="caption">
+	                    <h3>machine3</h3>
+	                    <p>choose the best in the world</p>
+	                  </div>
+	             </div>   
+	        </div>
+	    </div>
+  	</div>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,69 +1,17 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-   
-
-    <title>Hello, world!</title>
-    <link href="index.css" rel="stylesheet" type="text/css"/>
-    
-    <style type="text/css">
-  .nav-brand {
-    float: left;
-    margin-top: -20px;
-    height: 60px;
-    color: grey;
-    padding: 15px 5px;
-    font-size: 30px;
-    line-height: 30px;
-  }
-  
-</style>
-
-
-  </head>
-  <body>
-<div>
-
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+   <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+   <title>Hello, world!</title>
+   <link href="index.css" rel="stylesheet" type="text/css">
+</head>
+<body>
     <!-- this is for navigation bar -->
-<div>
-   
-   <nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class ="navbar-brand" href="#">
-        <img src="bnw.png" class="nav-brand" alt="logo" width="40px"></a>
-
-      <a class="navbar-brand" href="#">B.N.Electric</a>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Home</a></li>
-        <li><a href="#" data-toggle="modal" data-target="#contactus">Contact us</a></li>
-        <li><a href="#">Page 2</a></li>
-        <li><a href="#">Page 3</a></li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-user" data-toggle="modal" data-target="#modalContactForm"></span> Sign Up</a></li>
-        <li><a href="#"><span class="glyphicon glyphicon-log-in"data-toggle="modal" data-target="#pz">Login</span></a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
-
-
-<!-- this is for banner image -->
- 
-   <div class="content">
+    <div class="content">
     <div class="banner-image">
       <div class="inner-banner-image">
         <div class="banner_content">
@@ -74,184 +22,13 @@
       </div>
     </div>
   </div>
-
-
-                                             <!-- sign in form -->
-<div class="modal fade " id="pz" role="dialog">
-      <div class="modal-dialog ">
-
-          <div class="modal-content col-lg-6">
-              <div class="modal-header">
-                  <button type="button" class="close" data-dismiss="modal">&times;</button>
-                  <h4 class="modal-title">LOG IN</h4>
-              </div>
-              <div class="modal-body">
-                  <form method="post" action="logi.php">
-                      <div class="form-group ">
-                          <input type="email" name="email" class="form-control" placeholder="Email">
-                      </div>
-
-                      <div class="form-group">
-                          <input type="password" name="password" class="form-control" placeholder="Password">
-                      </div>
-                     <center> <button class="btn btn-primary" type="submit" value="submit"  name="button">Login</button></center>
-
-                  </form>
-              </div>
-          </div>
-          
-      </div>
-  </div>
-
-
-                                                            <!-- contact us form -->                         
-                                                            
-    
-
-                                                <!-- sign in form -->
-<div class="modal fade col-lg-6" id="contactus" role="dialog">
-      <div class="modal-dialog ">
-
-          <div class="modal-content col-lg-6">
-              <div class="modal-header">
-                  <button type="button" class="close" data-dismiss="modal">&times;</button>
-                  <h4 class="modal-title">CONTACT US</h4>
-              </div>
-              <div class="modal-body">
-                  <form method="post" action="logi.php">
-                    <div class="form-group">
-                          <input type="text" name="Name" class="form-control" placeholder="Name">
-                      </div>
-                      <div class="form-group ">
-                          <input type="email" name="email" class="form-control" placeholder="Email">
-                      </div>
-
-                      <div class="form-group">
-                          <input type="text" name="Address" class="form-control" placeholder="Address">
-                      </div>
-                      <div class="form-group">
-                          <textarea type="text" name="Subject" class="form-control" placeholder="Subject"></textarea>
-                      </div>
-                     <center> <button class="btn btn-primary" type="submit" value="submit"  name="button">Submit</button></center>
-
-                  </form>
-              </div>
-          </div>
-          
-      </div>
-  </div>
-
- <!-- 
-     
-                       footer -->
-
-<!-- 
-                       <div class="browser"> -->
-  <!--  <div class="chrome">
-    <a href="#" class="red"></a>
-    <a href="#" class="amber"></a>
-    <a href="#" class="green"></a>
-    <div class="url"></div>
-  </div>
-  <h1>Social media footer</h1>
-  <h2>Hover over the icons at the bottom of the page.</h2>
-  <footer>
-    <div class="social-media-links">
-      <ul>
-        <li>
-                                                           twitter -->
-         <!--  <a href="#">
-            <svg class="glow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
-              <path class="st0" d="M9.5 27.1c11.2 0 17.4-9.3 17.4-17.4 0-0.3 0-0.5 0-0.8 1.2-0.9 2.2-1.9 3-3.2 -1.1 0.5-2.3 0.8-3.5 1 1.3-0.8 2.2-2 2.7-3.4 -1.2 0.7-2.5 1.2-3.9 1.5 -1.1-1.2-2.7-1.9-4.5-1.9 -3.4 0-6.1 2.7-6.1 6.1 0 0.5 0.1 0.9 0.2 1.4C9.7 10.1 5.2 7.7 2.2 4 1.7 4.9 1.4 6 1.4 7.1c0 2.1 1.1 4 2.7 5.1 -1 0-1.9-0.3-2.8-0.8 0 0 0 0.1 0 0.1 0 3 2.1 5.4 4.9 6 -0.5 0.1-1.1 0.2-1.6 0.2 -0.4 0-0.8 0-1.1-0.1 0.8 2.4 3 4.2 5.7 4.2 -2.1 1.6-4.7 2.6-7.6 2.6 -0.5 0-1 0-1.5-0.1C2.8 26.1 6 27.1 9.5 27.1"/>
-            </svg>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
-              <path class="st0" d="M9.5 27.1c11.2 0 17.4-9.3 17.4-17.4 0-0.3 0-0.5 0-0.8 1.2-0.9 2.2-1.9 3-3.2 -1.1 0.5-2.3 0.8-3.5 1 1.3-0.8 2.2-2 2.7-3.4 -1.2 0.7-2.5 1.2-3.9 1.5 -1.1-1.2-2.7-1.9-4.5-1.9 -3.4 0-6.1 2.7-6.1 6.1 0 0.5 0.1 0.9 0.2 1.4C9.7 10.1 5.2 7.7 2.2 4 1.7 4.9 1.4 6 1.4 7.1c0 2.1 1.1 4 2.7 5.1 -1 0-1.9-0.3-2.8-0.8 0 0 0 0.1 0 0.1 0 3 2.1 5.4 4.9 6 -0.5 0.1-1.1 0.2-1.6 0.2 -0.4 0-0.8 0-1.1-0.1 0.8 2.4 3 4.2 5.7 4.2 -2.1 1.6-4.7 2.6-7.6 2.6 -0.5 0-1 0-1.5-0.1C2.8 26.1 6 27.1 9.5 27.1"/>
-            </svg>
-          </a>
-        </li>
-        <li> -->
-          <!-- facebook -->
-          <!-- <a href="#">
-            <svg class="glow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
-              <path class="st0" d="M28.3 0.1H1.7c-0.9 0-1.6 0.7-1.6 1.6v26.5c0 0.9 0.7 1.6 1.6 1.6H16V18.4h-3.9v-4.5H16v-3.3c0-3.9 2.4-5.9 5.8-5.9 1.6 0 3.1 0.1 3.5 0.2v4l-2.4 0c-1.9 0-2.2 0.9-2.2 2.2v2.9h4.5l-0.6 4.5h-3.9v11.5h7.6c0.9 0 1.6-0.7 1.6-1.6V1.7C29.9 0.8 29.2 0.1 28.3 0.1z"/>
-            </svg>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
-              <path class="st0" d="M28.3 0.1H1.7c-0.9 0-1.6 0.7-1.6 1.6v26.5c0 0.9 0.7 1.6 1.6 1.6H16V18.4h-3.9v-4.5H16v-3.3c0-3.9 2.4-5.9 5.8-5.9 1.6 0 3.1 0.1 3.5 0.2v4l-2.4 0c-1.9 0-2.2 0.9-2.2 2.2v2.9h4.5l-0.6 4.5h-3.9v11.5h7.6c0.9 0 1.6-0.7 1.6-1.6V1.7C29.9 0.8 29.2 0.1 28.3 0.1z"/>
-            </svg>
-          </a>
-        </li>
-        <li> -->
-          <!-- youtube -->
-          <!-- <a href="#">
-            <svg class="glow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
-              <path class="st0" d="M29.7 9c0 0-0.3-2.1-1.2-3 -1.1-1.2-2.4-1.2-3-1.3C21.3 4.5 15 4.5 15 4.5h0c0 0-6.3 0-10.5 0.3C3.9 4.8 2.6 4.8 1.5 6 0.6 6.9 0.3 9 0.3 9S0 11.4 0 13.9v2.3C0 18.6 0.3 21 0.3 21s0.3 2.1 1.2 3c1.1 1.2 2.6 1.2 3.3 1.3C7.2 25.5 15 25.6 15 25.6s6.3 0 10.5-0.3c0.6-0.1 1.9-0.1 3-1.3 0.9-0.9 1.2-3 1.2-3s0.3-2.4 0.3-4.9v-2.3C30 11.4 29.7 9 29.7 9zM11.9 18.9l0-8.4 8.1 4.2L11.9 18.9z"/>
-            </svg>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
-              <path class="st0" d="M29.7 9c0 0-0.3-2.1-1.2-3 -1.1-1.2-2.4-1.2-3-1.3C21.3 4.5 15 4.5 15 4.5h0c0 0-6.3 0-10.5 0.3C3.9 4.8 2.6 4.8 1.5 6 0.6 6.9 0.3 9 0.3 9S0 11.4 0 13.9v2.3C0 18.6 0.3 21 0.3 21s0.3 2.1 1.2 3c1.1 1.2 2.6 1.2 3.3 1.3C7.2 25.5 15 25.6 15 25.6s6.3 0 10.5-0.3c0.6-0.1 1.9-0.1 3-1.3 0.9-0.9 1.2-3 1.2-3s0.3-2.4 0.3-4.9v-2.3C30 11.4 29.7 9 29.7 9zM11.9 18.9l0-8.4 8.1 4.2L11.9 18.9z"/>
-            </svg>
-          </a>
-        </li>
-        <li> -->
-          <!-- instagram -->
-          <!-- <a href="#">
-            <svg class="glow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
-              <path class="st0" d="M15 2.8c4 0 4.4 0 6 0.1 1.4 0.1 2.2 0.3 2.8 0.5 0.7 0.3 1.2 0.6 1.7 1.1 0.5 0.5 0.8 1 1.1 1.7C26.8 6.8 27 7.6 27.1 9c0.1 1.6 0.1 2 0.1 6s0 4.4-0.1 6c-0.1 1.4-0.3 2.2-0.5 2.8 -0.3 0.7-0.6 1.2-1.1 1.7 -0.5 0.5-1 0.8-1.7 1.1 -0.5 0.2-1.3 0.4-2.8 0.5 -1.6 0.1-2 0.1-6 0.1s-4.4 0-6-0.1c-1.4-0.1-2.2-0.3-2.8-0.5 -0.7-0.3-1.2-0.6-1.7-1.1 -0.5-0.5-0.8-1-1.1-1.7C3.2 23.2 3 22.4 2.9 21c-0.1-1.6-0.1-2-0.1-6s0-4.4 0.1-6C3 7.6 3.2 6.8 3.4 6.2 3.7 5.5 4 5.1 4.5 4.5c0.5-0.5 1-0.8 1.7-1.1C6.8 3.2 7.6 3 9 2.9 10.6 2.8 11 2.8 15 2.8M15 0.2c-4 0-4.5 0-6.1 0.1C7.3 0.3 6.2 0.6 5.3 0.9c-1 0.4-1.8 0.9-2.6 1.7C1.8 3.5 1.3 4.3 0.9 5.3c-0.4 0.9-0.6 2-0.7 3.6C0.2 10.5 0.1 11 0.1 15c0 4 0 4.5 0.1 6.1 0.1 1.6 0.3 2.7 0.7 3.6 0.4 1 0.9 1.8 1.7 2.6 0.8 0.8 1.7 1.3 2.6 1.7 0.9 0.4 2 0.6 3.6 0.7 1.6 0.1 2.1 0.1 6.1 0.1s4.5 0 6.1-0.1c1.6-0.1 2.7-0.3 3.6-0.7 1-0.4 1.8-0.9 2.6-1.7 0.8-0.8 1.3-1.7 1.7-2.6 0.4-0.9 0.6-2 0.7-3.6 0.1-1.6 0.1-2.1 0.1-6.1s0-4.5-0.1-6.1c-0.1-1.6-0.3-2.7-0.7-3.6 -0.4-1-0.9-1.8-1.7-2.6 -0.8-0.8-1.7-1.3-2.6-1.7 -0.9-0.4-2-0.6-3.6-0.7C19.5 0.2 19 0.2 15 0.2L15 0.2z"/>
-              <path class="st0" d="M15 7.4c-4.2 0-7.6 3.4-7.6 7.6s3.4 7.6 7.6 7.6 7.6-3.4 7.6-7.6S19.2 7.4 15 7.4zM15 20c-2.7 0-5-2.2-5-5s2.2-5 5-5c2.7 0 5 2.2 5 5S17.7 20 15 20z"/>
-              <circle class="st0" cx="22.9" cy="7.1" r="1.8"/>
-            </svg>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
-              <path class="st0" d="M15 2.8c4 0 4.4 0 6 0.1 1.4 0.1 2.2 0.3 2.8 0.5 0.7 0.3 1.2 0.6 1.7 1.1 0.5 0.5 0.8 1 1.1 1.7C26.8 6.8 27 7.6 27.1 9c0.1 1.6 0.1 2 0.1 6s0 4.4-0.1 6c-0.1 1.4-0.3 2.2-0.5 2.8 -0.3 0.7-0.6 1.2-1.1 1.7 -0.5 0.5-1 0.8-1.7 1.1 -0.5 0.2-1.3 0.4-2.8 0.5 -1.6 0.1-2 0.1-6 0.1s-4.4 0-6-0.1c-1.4-0.1-2.2-0.3-2.8-0.5 -0.7-0.3-1.2-0.6-1.7-1.1 -0.5-0.5-0.8-1-1.1-1.7C3.2 23.2 3 22.4 2.9 21c-0.1-1.6-0.1-2-0.1-6s0-4.4 0.1-6C3 7.6 3.2 6.8 3.4 6.2 3.7 5.5 4 5.1 4.5 4.5c0.5-0.5 1-0.8 1.7-1.1C6.8 3.2 7.6 3 9 2.9 10.6 2.8 11 2.8 15 2.8M15 0.2c-4 0-4.5 0-6.1 0.1C7.3 0.3 6.2 0.6 5.3 0.9c-1 0.4-1.8 0.9-2.6 1.7C1.8 3.5 1.3 4.3 0.9 5.3c-0.4 0.9-0.6 2-0.7 3.6C0.2 10.5 0.1 11 0.1 15c0 4 0 4.5 0.1 6.1 0.1 1.6 0.3 2.7 0.7 3.6 0.4 1 0.9 1.8 1.7 2.6 0.8 0.8 1.7 1.3 2.6 1.7 0.9 0.4 2 0.6 3.6 0.7 1.6 0.1 2.1 0.1 6.1 0.1s4.5 0 6.1-0.1c1.6-0.1 2.7-0.3 3.6-0.7 1-0.4 1.8-0.9 2.6-1.7 0.8-0.8 1.3-1.7 1.7-2.6 0.4-0.9 0.6-2 0.7-3.6 0.1-1.6 0.1-2.1 0.1-6.1s0-4.5-0.1-6.1c-0.1-1.6-0.3-2.7-0.7-3.6 -0.4-1-0.9-1.8-1.7-2.6 -0.8-0.8-1.7-1.3-2.6-1.7 -0.9-0.4-2-0.6-3.6-0.7C19.5 0.2 19 0.2 15 0.2L15 0.2z"/>
-              <path class="st0" d="M15 7.4c-4.2 0-7.6 3.4-7.6 7.6s3.4 7.6 7.6 7.6 7.6-3.4 7.6-7.6S19.2 7.4 15 7.4zM15 20c-2.7 0-5-2.2-5-5s2.2-5 5-5c2.7 0 5 2.2 5 5S17.7 20 15 20z"/>
-              <circle class="st0" cx="22.9" cy="7.1" r="1.8"/>
-            </svg>
-          </a>
-        </li>
-      </ul>
-    </div>
-  </footer>
-</div> -->
- 
-
-    <div class="container-fluid">
-    <div class="row">
-         <div class="col-xs-4">
-             <div class="thumbnail">
-                <img src="m1.jpg" >
-                  <div class="caption">
-                    <h3>machin1</h3>
-                    <p>choose the best in the world</p>
-                  </div>
-            </div>
-        </div>
-
-        <div class="col-xs-4">
-             <div class="thumbnail">
-                <img src="m2.jpg" >
-                <div class="caption">
-                    <h3>machine2</h3>
-                    <p>choose the best in the world</p>
-                  </div>
-           </div>
-        </div>
-           
-        <div class="col-xs-4">
-             <div class="thumbnail">
-                <img src="m4.jpg" >
-                <div class="caption">
-                    <h3>machine3</h3>
-                    <p>choose the best in the world</p>
-                  </div>
-             </div>   
-        </div>
-    </div>
-
-
-  </div>
-
-
-
-
-
-    
+   
+   <?php
+   	require 'header.php';
+   	require 'signup.php';
+   	require 'contactus.php';
+   	require 'context.php';
+   ?>
   </body>
 </html>
+
